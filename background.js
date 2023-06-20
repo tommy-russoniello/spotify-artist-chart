b5804e92873b157c8ecb91dbc5a635633729224d
Tommy Russoniello <tommy.russoniello@gmail.com>
2023-06-19 21:55:18 -0400

    Use js standard style

--- background.js
+++ background.js
@@ -1,55 +1,55 @@
 chrome.webRequest.onSendHeaders.addListener(
-  function(details) {
-    if (!details.url.startsWith('https://api-partner.spotify.com/pathfinder/v1/query')) return;
+  function (details) {
+    if (!details.url.startsWith('https://api-partner.spotify.com/pathfinder/v1/query')) return
     for (var i = 0; i < details.requestHeaders.length; ++i) {
       if (details.requestHeaders[i].name.toLowerCase() === 'authorization') {
-        authToken = details.requestHeaders[i].value.match(/Bearer\ (.+)/)[1]
+        const authToken = details.requestHeaders[i].value.match(/Bearer (.+)/)[1]
         if (authToken && typeof authToken !== 'undefined' && details.initiator.match(/.*:\/\/.*spotify.com.*/)) {
-          chrome.storage.local.set({ auth: authToken}, function() {});
+          chrome.storage.local.set({ auth: authToken }, function () {})
         }
       }
     }
   },
   {
-    urls: ["<all_urls>"],
-    types: ["xmlhttprequest"]
+    urls: ['<all_urls>'],
+    types: ['xmlhttprequest']
   },
   ['requestHeaders']
-);
+)
 
 chrome.action.onClicked.addListener(function (tab) {
-  artist_id = tab.url.match(/https:\/\/open.spotify.com\/artist\/(.+)/)[1]
-  artist_name = tab.title.match(/(.+) \| Spotify/)[1]
+  const artistId = tab.url.match(/https:\/\/open.spotify.com\/artist\/(.+)/)[1]
+  const artistName = tab.title.match(/(.+) \| Spotify/)[1]
   chrome.storage.local.set(
-    { artist_id: artist_id, artist_name: artist_name},
-    function() {
+    { artistId: artistId, artistName: artistName },
+    function () {
       chrome.tabs.create({
         url: 'index.html',
-        selected: true,
+        selected: true
       })
-    });
+    })
 })
 
 // Wrap in an onInstalled callback in order to avoid unnecessary work
 // every time the background script is run
 chrome.runtime.onInstalled.addListener(() => {
   // Page actions are disabled by default and enabled on select tabs
-  chrome.action.disable();
+  chrome.action.disable()
 
   // Clear all rules to ensure only our expected rules are set
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     // Declare a rule to enable the action on example.com pages
-    let exampleRule = {
+    const exampleRule = {
       conditions: [
         new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: { hostSuffix: '.spotify.com'},
+          pageUrl: { hostSuffix: '.spotify.com' }
         })
       ],
-      actions: [new chrome.declarativeContent.ShowAction()],
-    };
+      actions: [new chrome.declarativeContent.ShowAction()]
+    }
 
     // Finally, apply our new array of rules
-    let rules = [exampleRule];
-    chrome.declarativeContent.onPageChanged.addRules(rules);
-  });
-});
+    const rules = [exampleRule]
+    chrome.declarativeContent.onPageChanged.addRules(rules)
+  })
+})
--- main.js
+++ main.js
@@ -2,32 +2,32 @@ const API_URL = 'https://api-partner.spotify.com/pathfinder/v1/query'
 const ARTIST_QUERY_SHA = 'e38c23e4e8aa873903ab47c2c84ab9f1175e645cf03a34eafdeea07454e5c3da'
 const ALBUM_QUERY_SHA = '3ea563e1d68f486d8df30f69de9dcedae74c77e684b889ba7408c589d30f7f2e'
 const SINGLE_QUERY_SHA = 'e02547d028482cec098d8d31899afcf488026d5dbdc2fcb973f05657c9cd6797'
-const REMASTER_TEXT = "((re-?)?master(ed)?|mix|mono|stereo|deluxe?|UK|US)(\\ (single\\ )?(version|edition|deluxe?))?"
-const SPECIAL_EDITION_TEXT = "(((\\d{1,3}(st|nd|rd|th)[\\w\\ ]*)|([\\w\\ ]*(super|deluxe?|special|UK|US)[\\w\\ ]*))(edition|version|deluxe?))"
-const REMASTER_REGEX = new RegExp(`\\ ?[-|\\(|\\/|;]\\ ?(${SPECIAL_EDITION_TEXT}\\ ?[-|\\/|;]?\\ ?)?((\\d{4}\\ ?${REMASTER_TEXT})|(${REMASTER_TEXT}\\ ?\\d{4})|${REMASTER_TEXT}|${SPECIAL_EDITION_TEXT})(\\ ?\\/\\ ${REMASTER_TEXT})?\\)?$`, "i")
+const REMASTER_TEXT = '((re-?)?master(ed)?|mix|mono|stereo|deluxe?|UK|US)(\\ (single\\ )?(version|edition|deluxe?))?'
+const SPECIAL_EDITION_TEXT = '(((\\d{1,3}(st|nd|rd|th)[\\w\\ ]*)|([\\w\\ ]*(super|deluxe?|special|UK|US)[\\w\\ ]*))(edition|version|deluxe?))'
+const REMASTER_REGEX = new RegExp(`\\ ?[-|\\(|\\/|;]\\ ?(${SPECIAL_EDITION_TEXT}\\ ?[-|\\/|;]?\\ ?)?((\\d{4}\\ ?${REMASTER_TEXT})|(${REMASTER_TEXT}\\ ?\\d{4})|${REMASTER_TEXT}|${SPECIAL_EDITION_TEXT})(\\ ?\\/\\ ${REMASTER_TEXT})?\\)?$`, 'i')
 
 $(document).ready(function () {
-  var auth;
-  var artist_id;
-  var debug;
+  var auth
+  var artistId
+  var debug
   function run () {
     chrome.storage.local.get([
       'debug',
       'auth',
-      'artist_id',
-      'artist_name'
-     ], function(result) {
+      'artistId',
+      'artistName'
+    ], function (result) {
       debug = result.debug === 'true'
       auth = result.auth
-      artist_id = result.artist_id
-      $(document).prop('title', `${result.artist_name} | Spotify Artist Play Count`);
+      artistId = result.artistId
+      $(document).prop('title', `${result.artistName} | Spotify Artist Play Count`)
       getDiscography()
-     })
+    })
   }
 
-  //----------------------------------------------------------------------------
+  // ----------------------------------------------------------------------------
   //                             Helpers
-  //----------------------------------------------------------------------------
+  // ----------------------------------------------------------------------------
 
   function assignTrackFields (track, trackData, name, release) {
     Object.assign(track, {
@@ -49,10 +49,10 @@ $(document).ready(function () {
   // 3. "(I Can't Get No) Satisfaction - Mono Version" =>
   // "(I Can't Get No) Satisfaction"
   function CleanUpName (name) {
-    return name.trim().normalize().replace("’", "'").replace(REMASTER_REGEX, "")
+    return name.trim().normalize().replace('’', "'").replace(REMASTER_REGEX, '')
   }
 
-  function debugLog(message) {
+  function debugLog (message) {
     if (!debug) return
 
     console.log(message)
@@ -63,26 +63,26 @@ $(document).ready(function () {
     $('#error').show()
   }
 
-  function getFirstWordOfString(string) {
-    return string.match(/^[^.,\/#!$%\^&\*;:{}=`~()\s]*/)?.at(0)?.toLowerCase()
+  function getFirstWordOfString (string) {
+    return string.match(/^[^.,/#!$%^&*;:{}=`~()\s]*/)?.at(0)?.toLowerCase()
   }
 
-  function makeRequest ({uri, limit, querySha, action, success, error, context}={}) {
+  function makeRequest ({ uri, limit, querySha, action, success, error, context } = {}) {
     const variables = {
-      "uri": uri,
-      "offset": 0,
-      "limit": limit
+      uri: uri,
+      offset: 0,
+      limit: limit
     }
     const extensions = {
-      "persistedQuery": {
-        "version": 1,
-        "sha256Hash": querySha
+      persistedQuery: {
+        version: 1,
+        sha256Hash: querySha
       }
     }
-    const url = new URL(API_URL);
-    url.searchParams.append("operationName", action)
-    url.searchParams.append("variables", JSON.stringify(variables))
-    url.searchParams.append("extensions", JSON.stringify(extensions))
+    const url = new URL(API_URL)
+    url.searchParams.append('operationName', action)
+    url.searchParams.append('variables', JSON.stringify(variables))
+    url.searchParams.append('extensions', JSON.stringify(extensions))
 
     return $.ajax({
       url: url,
@@ -100,9 +100,9 @@ $(document).ready(function () {
   function recordRemovedTrackForDebug (tracks, track) {
     if (!debug) return
 
-    let removed_track = Object.assign({}, track)
-    removed_track.removed = true
-    tracks.push(removed_track)
+    const removedTrack = Object.assign({}, track)
+    removedTrack.removed = true
+    tracks.push(removedTrack)
   }
 
   // Replace track if any of the following is true:
@@ -110,73 +110,73 @@ $(document).ready(function () {
   // 2. The existing track was released before the new one.
   // 3. The tracks have the same release date, but the existing one has a longer
   // name (meaning it is likely some redone version).
-  function shouldReplaceTrack (existing_track, new_track) {
-    if (existing_track.release.single && !new_track.release.single) return true
-    if (existing_track.release.date > new_track.release.date) return true
-    if (existing_track.release.date != new_track.release.date) return false
-    return existing_track.raw_name.length >= new_track.raw_name.length
+  function shouldReplaceTrack (existingTrack, newTrack) {
+    if (existingTrack.release.single && !newTrack.release.single) return true
+    if (existingTrack.release.date > newTrack.release.date) return true
+    if (existingTrack.release.date !== newTrack.release.date) return false
+    return existingTrack.raw_name.length >= newTrack.raw_name.length
   }
 
   // Tracks have similar durations if they're within 10 seconds of eachother.
-  function tracksHaveSimilarDurations(track_a, track_b) {
-    return Math.abs(track_a.duration - track_b.duration) < 10000
+  function tracksHaveSimilarDurations (trackA, trackB) {
+    return Math.abs(trackA.duration - trackB.duration) < 10000
   }
 
   // Tracks have similar names if they start with the same word.
-  function tracksHaveSimilarNames(track_a, track_b) {
-    const first_word = getFirstWordOfString(track_a.name)
-    return first_word && first_word == getFirstWordOfString(track_b.name)
+  function tracksHaveSimilarNames (trackA, trackB) {
+    const firstWord = getFirstWordOfString(trackA.name)
+    return firstWord && firstWord === getFirstWordOfString(trackB.name)
   }
 
   // Tracks have similar plays if they have measurable plays (tracks with <1,000
   // plays show as having 0) and the difference between the more played one and
   // less played one is less than 0.1% of the more played one's plays.
-  function tracksHaveSimilarPlays(track_a, track_b) {
-    return track_a.plays > 0 && Math.abs(track_a.plays - track_b.plays) <
-      Math.max(track_a.plays, track_b.plays) * 0.001
+  function tracksHaveSimilarPlays (trackA, trackB) {
+    return trackA.plays > 0 && Math.abs(trackA.plays - trackB.plays) <
+      Math.max(trackA.plays, trackB.plays) * 0.001
   }
 
-  //----------------------------------------------------------------------------
+  // ----------------------------------------------------------------------------
 
   function getDiscography () {
     var releases = []
     const promises = [
       makeRequest({
-        uri: `spotify:artist:${artist_id}`,
+        uri: `spotify:artist:${artistId}`,
         limit: 100,
         querySha: ARTIST_QUERY_SHA,
-        action: "queryArtistDiscographyAlbums",
-        success: function(data) {
-          albumsData = data.data.artist.discography.albums.items
+        action: 'queryArtistDiscographyAlbums',
+        success: function (data) {
+          const albumsData = data.data.artist.discography.albums.items
           albumsData.forEach(albumData => {
             releases.push(albumData.releases.items[0])
           })
         },
-        error: function(data) {
+        error: function (data) {
           console.log(data)
           displayError(data.responseText)
         }
       }),
       makeRequest({
-        uri: `spotify:artist:${artist_id}`,
+        uri: `spotify:artist:${artistId}`,
         limit: 100,
         querySha: SINGLE_QUERY_SHA,
-        action: "queryArtistDiscographySingles",
-        success: function(data) {
-          albumsData = data.data.artistUnion.discography.singles.items
+        action: 'queryArtistDiscographySingles',
+        success: function (data) {
+          const albumsData = data.data.artistUnion.discography.singles.items
           albumsData.forEach(albumData => {
-            releases.push(Object.assign(albumData.releases.items[0], {single: true}))
+            releases.push(Object.assign(albumData.releases.items[0], { single: true }))
           })
         },
-        error: function(data) {
+        error: function (data) {
           console.log(data)
           displayError(data.responseText)
         }
       })
     ]
-    $.when.apply($, promises).then(function() {
+    $.when.apply($, promises).then(function () {
       getTracks(releases)
-    });
+    })
   }
 
   function getTracks (discography) {
@@ -190,7 +190,7 @@ $(document).ready(function () {
           uri: `spotify:album:${discography[i].id}`,
           limit: 300,
           querySha: ALBUM_QUERY_SHA,
-          action: "queryAlbumTracks",
+          action: 'queryAlbumTracks',
           context: {
             releaseData: {
               date: new Date(discography[i].date.isoString),
@@ -199,7 +199,7 @@ $(document).ready(function () {
               single: discography[i].single
             }
           },
-          success: function(data) {
+          success: function (data) {
             releases.push({
               name: this.releaseData.name,
               date: this.releaseData.date,
@@ -211,87 +211,88 @@ $(document).ready(function () {
         })
       )
     }
-    $.when.apply($, promises).then(function() {
+    $.when.apply($, promises).then(function () {
       debugLog(releases)
       var tracks = []
       var tracksByPlayCount = {}
       var tracksByName = {}
       releases.forEach(release => {
-        // maybe make it so you can see a per album view? (like total plays per album)
+        // maybe make it so you can see a per album view?
+        // (like total plays per album)
         release.tracks.forEach(trackData => {
-          new_track = {}
-          const track_name = CleanUpName(trackData.track.name)
-          assignTrackFields(new_track, trackData, track_name, release)
-          
+          const newTrack = {}
+          const trackName = CleanUpName(trackData.track.name)
+          assignTrackFields(newTrack, trackData, trackName, release)
+
           // If this track is a duplicate of one that's already been added,
           // only keep the better one.
-          existing_track = tracksByPlayCount[new_track.plays]
-          if (existing_track &&
-            (tracksHaveSimilarNames(existing_track, new_track) &&
-            tracksHaveSimilarDurations(existing_track, new_track))) {
-            if (shouldReplaceTrack(existing_track, new_track)) {
-              recordRemovedTrackForDebug(tracks, existing_track)
-              assignTrackFields(existing_track, trackData, new_track.name, new_track.release)
+          let existingTrack = tracksByPlayCount[newTrack.plays]
+          if (existingTrack &&
+            (tracksHaveSimilarNames(existingTrack, newTrack) &&
+            tracksHaveSimilarDurations(existingTrack, newTrack))) {
+            if (shouldReplaceTrack(existingTrack, newTrack)) {
+              recordRemovedTrackForDebug(tracks, existingTrack)
+              assignTrackFields(existingTrack, trackData, newTrack.name, newTrack.release)
             } else {
-              recordRemovedTrackForDebug(tracks, new_track)
+              recordRemovedTrackForDebug(tracks, newTrack)
             }
 
             return
           }
 
-          existing_track = tracksByName[new_track.name]
-          if (existing_track && tracksHaveSimilarPlays(existing_track, new_track)) {
-            if (shouldReplaceTrack(existing_track, new_track)) {
-              recordRemovedTrackForDebug(tracks, existing_track)
-              assignTrackFields(existing_track, trackData, new_track.name, new_track.release)
+          existingTrack = tracksByName[newTrack.name]
+          if (existingTrack && tracksHaveSimilarPlays(existingTrack, newTrack)) {
+            if (shouldReplaceTrack(existingTrack, newTrack)) {
+              recordRemovedTrackForDebug(tracks, existingTrack)
+              assignTrackFields(existingTrack, trackData, newTrack.name, newTrack.release)
             } else {
-              recordRemovedTrackForDebug(tracks, new_track)
+              recordRemovedTrackForDebug(tracks, newTrack)
             }
 
             return
           }
 
-          tracksByName[new_track.name] = new_track
-          tracksByPlayCount[new_track.plays] = new_track
-          tracks.push(new_track)
+          tracksByName[newTrack.name] = newTrack
+          tracksByPlayCount[newTrack.plays] = newTrack
+          tracks.push(newTrack)
         })
       })
 
       debugLog(tracks)
       displayTracks(tracks)
-    }, function(data) {
+    }, function (data) {
       console.log(data)
       displayError(data.responseText)
-    });
+    })
   }
 
   function displayTracks (tracks) {
-    tracks.sort(function(a, b) {
-      return b.plays - a.plays;
-    });
-    removed_count = 0
+    tracks.sort(function (a, b) {
+      return b.plays - a.plays
+    })
+    let removedCount = 0
     for (var i = 0; i < tracks.length; i++) {
       const track = tracks[i]
-      if (track.removed) removed_count += 1
+      if (track.removed) removedCount += 1
       // Format number with commas
-      playCountString = track.plays.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-      $("#table").find("tbody")
-        .append($('<tr>').addClass(track.removed ? "removed" : "")
-            .append($('<td>').text(track.removed ? "X" : i + 1 - removed_count).addClass("row-number"))
-            .append($('<td>').text(track.name))
-            .append($('<td>')
-              .append($('<a>')
-                .attr("target", "_blank")
-                .attr("href", track.release.link)
-                .text(track.release.name)
-              )
+      const playCountString = track.plays.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+      $('#table').find('tbody')
+        .append($('<tr>').addClass(track.removed ? 'removed' : '')
+          .append($('<td>').text(track.removed ? 'X' : i + 1 - removedCount).addClass('row-number'))
+          .append($('<td>').text(track.name))
+          .append($('<td>')
+            .append($('<a>')
+              .attr('target', '_blank')
+              .attr('href', track.release.link)
+              .text(track.release.name)
             )
-            .append($('<td>').text(playCountString).attr('data-sort', track.plays))
-            .append($('<td>').text(track.id))
+          )
+          .append($('<td>').text(playCountString).attr('data-sort', track.plays))
+          .append($('<td>').text(track.id))
         )
     }
 
-    $(".table").show()
+    $('.table').show()
   }
 
   run()
@@ -299,18 +300,18 @@ $(document).ready(function () {
 
 document.addEventListener('click', function (e) {
   try {
-    function findElementRecursive(element, tag) {
+    function findElementRecursive (element, tag) {
       return element.nodeName === tag ? element : findElementRecursive(element.parentNode, tag)
     }
 
-    const regex_table = /\bsortable\b/
+    const regexTable = /\bsortable\b/
     const element = findElementRecursive(e.target, 'TH')
     const tr = findElementRecursive(element, 'TR')
     const table = findElementRecursive(tr, 'TABLE')
 
-    if (regex_table.test(table.className)) {
+    if (regexTable.test(table.className)) {
       var i = 1
-      $(".row-number").each(function () {
+      $('.row-number').each(function () {
         $(this).html(i)
         i++
       })
